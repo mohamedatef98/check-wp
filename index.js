27f255632dad68cca1ee5efb510ea9ebaf080ff3
Mohamed Atef Abdel Samie <eng.mohamedatef98@gmail.com>
2023-07-29 19:30:45 +0300

    Prints application status

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
 
 const MV_URL =
   "https://www.migrationsverket.se/English/Contact-us/Check-your-application-without-logging-in.html";
-const CASE_NUMBER = 52567092;
+const CASE_NUMBER = 52567090;
+const STATUS_BOX_SELECTOR = By.css('.information-box a');
 
 (async function example() {
   let driver = await new Builder().forBrowser(Browser.CHROME).build();
   try {
     await driver.get(MV_URL);
     await driver.findElement(By.css(`input[type="number"]`)).sendKeys(CASE_NUMBER, Key.RETURN);
-    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    await driver.wait(until.elementLocated(STATUS_BOX_SELECTOR), 5000);
+    const title = await driver.findElement(STATUS_BOX_SELECTOR).getText()
+    console.log(title.trim().split(" "))
   } finally {
     await driver.quit();
   }
