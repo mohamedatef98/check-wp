2277aca31d32091a756d22adf38c3edc423d785b
Mohamed Atef <eng.mohamedatef98@gmail.com>
2023-07-29 18:01:44 +0000

    Support running onserver and headless

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
 
 const MV_URL =
   "https://www.migrationsverket.se/English/Contact-us/Check-your-application-without-logging-in.html";
@@ -6,7 +7,15 @@ const CASE_NUMBER = 52567090;
 const STATUS_BOX_SELECTOR = By.css('.information-box a');
 
 (async function example() {
-  let driver = await new Builder().forBrowser(Browser.CHROME).build();
+  const chromeOptions = new chrome.Options();
+
+  chromeOptions.headless();
+  chromeOptions.addArguments("--no-sandbox");
+
+  let driver = await new Builder()
+                          .forBrowser(Browser.CHROME)
+                          .setChromeOptions(chromeOptions)
+                          .build();
   try {
     await driver.get(MV_URL);
     await driver.findElement(By.css(`input[type="number"]`)).sendKeys(CASE_NUMBER, Key.RETURN);
